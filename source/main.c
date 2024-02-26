c9e55f9f298fbc1f4f7ccd25958f0697b860d0e2
Hugo <hugo.cathelain@epitech.eu>
2024-02-26 19:44:26 +0100

    [ADD] option --help to undersatnd how to use the program or have a description

--- source/main.c
+++ source/main.c
@@ -6,8 +6,18 @@
 */
 #include "paint.h"
 
+int print_help(void)
+{
+    my_putstr("Usage :\n\t./my_paint\n\nDescription :\n\tYou can use this pr");
+    my_putstr("ogram for painting or changing an image from your device to y");
+    my_putstr("our liking and so on.\n");
+    return 0;
+}
+
 int main(int argc, warray argv)
 {
+    if (argc == 2)
+        RETURN(my_strcmp(argv[1], "--help") == 0, print_help());
     window_init();
     time_init();
     window_destroy();
