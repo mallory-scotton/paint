4239fdfca34b68bb3a5bf054caf58ba6d1856d34
Hugo <hugo.cathelain@epitech.eu>
2024-02-26 19:34:44 +0100

    [ADD] main function with windows init and time init

---
+++ source/main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-MUL-200-LYN-2-1-mypaint-mallory.scotton
+** File description:
+** main
+*/
+#include "paint.h"
+
+int main(int argc, warray argv)
+{
+    window_init();
+    time_init();
+    window_destroy();
+    time_destroy();
+    return 0;
+}
